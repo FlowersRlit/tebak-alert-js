937e025023aa112b287dae2584d7fd69ccce46f0
FlowersRlit <yourstrulygurl@gmail.com>
2023-03-10 06:34:00 +0700

    Finilize question and input answer

--- public/data.js
+++ public/data.js
@@ -1,3 +1,25 @@
-/* 
-I want to make javascript dialogue that will interact with user just like a minigame.
-*/
+alert("Halo. Aku ada pertanyaan nih :)");
+welcomeBox();
+
+function welcomeBox() {
+  const inputan = prompt("Kenalan dulu. Kamu siapa namanya?", "");
+  if (inputan != NaN) {
+    alert(`Oh, ${inputan}. Salam kenal ya!.`);
+    questionBox();
+  } else {
+    alert("Kayaknya salah ya, coba lagi deh..");
+    return;
+  }
+}
+
+function questionBox() {
+  alert("Oke, pertanyaannya tuh gini.");
+  const q1 = prompt("Sayur-sayur apa yang dingin? ").toLowerCase();
+  if (q1 == "kembang cold") {
+    window.location.href = "./true.html";
+    alert("Selamat!");
+  } else {
+    window.location.href = "./false.html";
+    alert("Yah, jawabannya salah");
+  }
+}
